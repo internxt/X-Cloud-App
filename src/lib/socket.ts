a27b5249b315e672225e0739a053ae461bda826f
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-02-08 14:09:26 +0100

    socket utils

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -5,3 +5,20 @@ export const socket = io(process.env.NODE_ENV === 'production' ? '' : 'http://lo
     path: '/api/sockets',
     timeout: 60000 
 });
+
+export const reconnect = ({ wait }) : Promise<boolean> => {
+    socket.connect();
+    return new Promise((res) => {
+        setTimeout(() => res(false), wait * 1000); 
+        socket.on('connect', () => res(true));
+    });
+}
+
+export const isInternetWorking = async () : Promise<boolean> => {
+    try {
+        await fetch('www.google.com');
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
