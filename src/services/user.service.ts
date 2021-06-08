e3beef82f5866d25f4472e65c3d2cabceaa9628f
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-06-08 13:05:58 +0200

    lint fix in user.service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,13 @@
-import { getHeaders } from "../lib/auth";
+import { getHeaders } from '../lib/auth';
+import Settings from '../lib/settings';
 
 export function isUserActivated() {
-    return fetch('/api/user/isactivated', {
-        method: 'get',
-        headers: getHeaders(true, false)
-    }).then((response) => {
-        return response.json()
-    }).catch(() => {
-        console.log('Error getting user activation');
-    });
+  return fetch('/api/user/isactivated', {
+    method: 'get',
+    headers: getHeaders(true, false)
+  }).then((response) => {
+    return response.json();
+  }).catch(() => {
+    console.log('Error getting user activation');
+  });
 }
